75823ea4d1df12f16df5943170557e25cedda19a
ardamoin <ardamoin2@gmail.com>
2023-05-04 21:32:45 -0400

    Make addExperience button component

---
+++ src/Components/UI/AddExperience.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Tooltip } from "react-tooltip";
+
+import classes from "./AddExperience.module.css";
+
+import addBtn from "../../Assets/addBtn.svg";
+
+const AddExperience = (props) => {
+  return (
+    <>
+      <img
+        src={addBtn}
+        alt="add experience button"
+        className={classes["add-btn"]}
+        data-tooltip-id="add-btn"
+        data-tooltip-content="Add experience"
+        data-tooltip-place="bottom"
+        onClick={props.onAddExperience}
+      />
+      <Tooltip id="add-btn" />
+    </>
+  );
+};
+
+export default AddExperience;
